f33322a1a8e66436fcfaf77facc672326e5afb6d
fatihyaylacimk <fatihyaylaci7@gmail.com>
2025-12-27 22:19:18 +0300

    Integrate AirAgent and update movement logic
    Added AirAgent functionality and modified pathfinding for both land and air agents.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from agents.agent import Agent
+from agents.air_agent import AirAgent
 from environment.map import GridMap
 from algorithms.bfs import bfs
 
@@ -6,7 +7,7 @@ if __name__ == "__main__":
     grid = GridMap(width=5, height=5)
     grid.add_obstacle(2, 2)
 
-    agent1 = Agent(
+    land_agent = Agent(
         agent_id=1,
         agent_type="LAND",
         x=0,
@@ -14,14 +15,25 @@ if __name__ == "__main__":
         speed=1
     )
 
-    start = agent1.position()
+    air_agent = AirAgent(
+        agent_id=2,
+        x=0,
+        y=4,
+        speed=2
+    )
+
     goal = (4, 4)
 
-    path = bfs(grid, start, goal)
-    print("Path:", path)
+    # LAND agent BFS ile gider
+    path = bfs(grid, land_agent.position(), goal)
+    print("LAND path:", path)
 
     for step in path[1:]:
-        dx = step[0] - agent1.x
-        dy = step[1] - agent1.y
-        agent1.move(dx, dy, grid)
-        print("Moved to:", agent1.position())
+        dx = step[0] - land_agent.x
+        dy = step[1] - land_agent.y
+        land_agent.move(dx, dy, grid)
+        print("LAND moved to:", land_agent.position())
+
+    # AIR agent direkt gider
+    air_agent.move(goal[0] - air_agent.x, goal[1] - air_agent.y, grid)
+    print("AIR moved to:", air_agent.position())
