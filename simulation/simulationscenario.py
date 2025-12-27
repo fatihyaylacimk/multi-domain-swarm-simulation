91158b8f0eef374c58cca4996336c48ddce306ef
fatihyaylacimk <fatihyaylaci7@gmail.com>
2025-12-27 22:24:55 +0300

    Create simulationscenario.py

---
+++ simulation/simulationscenario.py
@@ -0,0 +1,11 @@
+def load_scenario():
+    # Basit senaryo tanımı
+    scenario = {
+        "map": {
+            "width": 5,
+            "height": 5,
+            "obstacles": [(2, 2)]
+        },
+        "goal": (4, 4)
+    }
+    return scenario
