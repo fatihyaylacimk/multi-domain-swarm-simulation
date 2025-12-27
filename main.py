5857208decbe3bed16db1b74e03e1467b2ccc321
fatihyaylacimk <fatihyaylaci7@gmail.com>
2025-12-27 22:20:35 +0300

    Refactor agent initialization and movement logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from agents.agent import Agent
+
+  from agents.agent import Agent
 from agents.air_agent import AirAgent
 from environment.map import GridMap
 from algorithms.bfs import bfs
@@ -7,33 +8,30 @@ if __name__ == "__main__":
     grid = GridMap(width=5, height=5)
     grid.add_obstacle(2, 2)
 
-    land_agent = Agent(
-        agent_id=1,
-        agent_type="LAND",
-        x=0,
-        y=0,
-        speed=1
-    )
+    # LAND swarm
+    land_agents = [
+        Agent(1, "LAND", 0, 0, 1),
+        Agent(2, "LAND", 0, 1, 1),
+        Agent(3, "LAND", 1, 0, 1),
+    ]
 
-    air_agent = AirAgent(
-        agent_id=2,
-        x=0,
-        y=4,
-        speed=2
-    )
+    # AIR agent
+    air_agent = AirAgent(4, 0, 4, 2)
 
     goal = (4, 4)
 
-    # LAND agent BFS ile gider
-    path = bfs(grid, land_agent.position(), goal)
-    print("LAND path:", path)
+    # LAND agents BFS ile gider
+    for agent in land_agents:
+        path = bfs(grid, agent.position(), goal)
+        print(f"LAND {agent.agent_id} path:", path)
 
-    for step in path[1:]:
-        dx = step[0] - land_agent.x
-        dy = step[1] - land_agent.y
-        land_agent.move(dx, dy, grid)
-        print("LAND moved to:", land_agent.position())
+        for step in path[1:]:
+            dx = step[0] - agent.x
+            dy = step[1] - agent.y
+            agent.move(dx, dy, grid)
+
+        print(f"LAND {agent.agent_id} final:", agent.position())
 
     # AIR agent direkt gider
     air_agent.move(goal[0] - air_agent.x, goal[1] - air_agent.y, grid)
-    print("AIR moved to:", air_agent.position())
+    print("AIR final:", air_agent.position())
