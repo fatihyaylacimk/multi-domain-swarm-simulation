1a5a4b393f7de56fd5cedc041b4377b218d57c05
fatihyaylacimk <fatihyaylaci7@gmail.com>
2025-12-31 18:26:17 +0300

    Configure matplotlib backend and update render_visual
    Added matplotlib backend configuration and refactored render_visual method.

--- environment/map.py
+++ environment/map.py
@@ -1,3 +1,18 @@
+import matplotlib
+matplotlib.use("TkAgg")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+
+
+
 import matplotlib.pyplot as plt
 import numpy as np
     def render_visual(self, agents=None, goal=None):
@@ -29,3 +44,28 @@ plt.imshow(grid, cmap="viridis")
 plt.title("Multi-Domain Swarm Simulation")
 plt.grid(True)
 plt.show(block=True)
+
+def render_visual(self, agents=None, goal=None):
+    grid = np.zeros((self.height, self.width))
+
+    # Obstacles
+    for (x, y) in self.obstacles:
+        grid[y][x] = 1
+
+    # Goal
+    if goal:
+        gx, gy = goal
+        grid[gy][gx] = 3
+
+    # Agents
+    if agents:
+        for a in agents:
+            grid[a.y][a.x] = 2
+
+    plt.figure(figsize=(6, 6))
+    plt.imshow(grid, cmap="viridis")
+    plt.title("Multi-Domain Swarm Simulation")
+    plt.grid(True)
+    plt.show(block=True)
+
+
