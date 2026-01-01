82ae290dfa7a016ad49850c77b2614e61f609402
fatihyaylacimk <fatihyaylaci7@gmail.com>
2026-01-01 21:47:21 +0300

    Update agent.py

--- agents/agent.py
+++ agents/agent.py
@@ -5,3 +5,10 @@ class Agent:
         self.goal = goal
         self.path = []
         self.final = None
+
+    def set_path(self, path):
+        self.path = path
+        if path:
+            self.final = path[-1]
+        else:
+            self.final = None
