cbec60c093ab9a4c23ad00e9722561d29c9e9d6b
fatihyaylacimk <fatihyaylaci7@gmail.com>
2026-01-01 22:16:38 +0300

    Update agent.py

--- agents/agent.py
+++ agents/agent.py
@@ -1,9 +1,3 @@
-print("### AGENT.PY LOADED ###")
-
-
-
-
-
 class Agent:
     def __init__(self, name, start, goal):
         self.name = name
@@ -11,10 +5,19 @@ class Agent:
         self.goal = goal
         self.path = []
         self.final = None
+        self.step_index = 0
 
     def set_path(self, path):
         self.path = path
+        self.step_index = 0
         if path:
             self.final = path[-1]
         else:
             self.final = None
+
+    def move_step(self):
+        if self.step_index < len(self.path):
+            pos = self.path[self.step_index]
+            self.step_index += 1
+            return pos
+        return None
