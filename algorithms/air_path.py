864f7cb3ad33b48951cc848413073070e7808bff
fatihyaylacimk <fatihyaylaci7@gmail.com>
2026-01-02 21:47:42 +0300

    Update air_path.py

--- algorithms/air_path.py
+++ algorithms/air_path.py
@@ -4,11 +4,15 @@ def air_path(start, goal):
     path = []
 
     while (x, y) != (gx, gy):
-        if x < gx: x += 1
-        elif x > gx: x -= 1
+        if x < gx:
+            x += 1
+        elif x > gx:
+            x -= 1
 
-        if y < gy: y += 1
-        elif y > gy: y -= 1
+        if y < gy:
+            y += 1
+        elif y > gy:
+            y -= 1
 
         path.append((x, y))
 
